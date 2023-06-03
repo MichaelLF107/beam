87d0453d53f3e3bc106d61920a90453d695e34da
Michael Friedrich <michaelfriedrich253@gmail.com>
2023-06-03 17:01:16 -0300

    first commit 🧙‍♂️

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
